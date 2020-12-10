35ea3d0cc17c581e8afa1836ae72e0ac1e36120a
Max Beneke <max.beneke@gmail.com>
2020-12-10 14:17:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,5 +18,6 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.include? valid_calls.count
+  planeteer_calls.find do |element|
+    valid_calls.include? element
 end
